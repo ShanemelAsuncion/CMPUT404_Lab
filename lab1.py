012e9bbe80254b1a33ca8396f5692c36e9eeeb64
ShanemelAsuncion <shanemel@ualberta.ca>
2023-09-13 18:16:47 -0600

    new changes

--- lab1.py
+++ lab1.py
@@ -1,5 +1,5 @@
 import requests
 
 #print(requests.__version__)
-resp = requests.get("http://google.com")
+resp = requests.get("https://raw.githubusercontent.com/ShanemelAsuncion/CMPUT404_Lab/master/lab1.py")
 print(resp.text)
